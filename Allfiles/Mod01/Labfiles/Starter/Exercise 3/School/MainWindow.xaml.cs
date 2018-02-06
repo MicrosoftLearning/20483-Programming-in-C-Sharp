09ab11c6ad09f7fee854db059fe8fa05727a9842
Roi Godelman <roi1godelman@gmail.com>
2018-02-06 13:47:22 +0200

    Add Model 1

---
+++ Allfiles/Mod01/Labfiles/Starter/Exercise 3/School/MainWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using School.Data;
+
+
+namespace School
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Connection to the School database
+        private SchoolDBEntities schoolContext = null;
+
+        // Field for tracking the currently selected teacher
+        private Teacher teacher = null;
+
+        // List for tracking the students assigned to the teacher's class
+        private IList studentsInfo = null;
+
+        #region Predefined code
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Connect to the database and display the list of teachers when the window appears
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.schoolContext = new SchoolDBEntities();
+            teachersList.DataContext = this.schoolContext.Teachers;
+        }
+
+        // When the user selects a different teacher, fetch and display the students for that teacher
+        private void teachersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Find the teacher that has been selected
+            this.teacher = teachersList.SelectedItem as Teacher;
+            this.schoolContext.LoadProperty<Teacher>(this.teacher, s => s.Students);
+
+            // Find the students for this teacher
+            this.studentsInfo = ((IListSource)teacher.Students).GetList();
+
+            // Use databinding to display these students
+            studentsList.DataContext = this.studentsInfo;
+        }
+
+        #endregion
+
+        // When the user presses a key, determine whether to add a new student to a class, remove a student from a class, or modify the details of a student
+        private void studentsList_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                // If the user pressed Enter, edit the details for the currently selected student
+                case Key.Enter: Student student = this.studentsList.SelectedItem as Student;
+
+                    // Use the StudentsForm to display and edit the details of the student
+                    StudentForm sf = new StudentForm();
+
+                    // Set the title of the form and populate the fields on the form with the details of the student           
+                    sf.Title = "Edit Student Details";
+                    sf.firstName.Text = student.FirstName;
+                    sf.lastName.Text = student.LastName;
+                    sf.dateOfBirth.Text = student.DateOfBirth.ToString("d"); // Format the date to omit the time element
+
+                    // Display the form
+                    if (sf.ShowDialog().Value)
+                    {
+                        // When the user closes the form, copy the details back to the student
+                        student.FirstName = sf.firstName.Text;
+                        student.LastName = sf.lastName.Text;
+                        student.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
+
+                        // Enable saving (changes are not made permanent until they are written back to the database)
+                        saveChanges.IsEnabled = true;
+                    }
+                    break;
+
+                // If the user pressed Insert, add a new student
+                case Key.Insert:
+
+                    // Use the StudentsForm to get the details of the student from the user
+                    sf = new StudentForm();
+
+                    // Set the title of the form to indicate which class the student will be added to (the class for the currently selected teacher)
+                    sf.Title = "New Student for Class " + teacher.Class;
+
+                    // Display the form and get the details of the new student
+                    if (sf.ShowDialog().Value)
+                    {
+                        // When the user closes the form, retrieve the details of the student from the form
+                        // and use them to create a new Student object
+                        Student newStudent = new Student();
+                        newStudent.FirstName = sf.firstName.Text;
+                        newStudent.LastName = sf.lastName.Text;
+                        newStudent.DateOfBirth = DateTime.Parse(sf.dateOfBirth.Text);
+
+                        // Assign the new student to the current teacher
+                        this.teacher.Students.Add(newStudent);
+
+                        // Add the student to the list displayed on the form
+                        this.studentsInfo.Add(newStudent);
+
+                        // Enable saving (changes are not made permanent until they are written back to the database)
+                        saveChanges.IsEnabled = true;
+                    }
+                    break;
+
+                    // TODO: Exercise 3: Task 1a: If the user pressed Delete, remove the currently selected student
+                    // TODO: Exercise 3: Task 2a: Prompt the user to confirm that the student should be removed
+                    // TODO: Exercise 3: Task 3a: If the user clicked Yes, remove the student from the database
+                    // TODO: Exercise 3: Task 3b: Enable saving (changes are not made permanent until they are written back to the database)
+            }
+        }
+
+        #region Predefined code
+
+        private void studentsList_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+ 
+        }
+
+        // Save changes back to the database and make them permanent
+        private void saveChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
+    }
+
+    [ValueConversion(typeof(string), typeof(Decimal))]
+    class AgeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+                              System.Globalization.CultureInfo culture)
+        {
+            return "";
+        }
+
+        #region Predefined code
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
